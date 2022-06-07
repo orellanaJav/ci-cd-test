1aa76e811b0dd5cb1e047dd860ca9004fbe85acc
Javier Orellana <javier.orellana@destacame.cl>
2022-06-07 11:19:26 -0400

    test

--- stack_project_2/services/lambda_1/core/lambda_1.py
+++ stack_project_2/services/lambda_1/core/lambda_1.py
@@ -4,10 +4,7 @@ from settings.config import ANOTHER_KEY
 
 
 def hello(event, lambda_context):
-<<<<<<< HEAD
     print('hola')
-=======
->>>>>>> f2fb6456151b6adaff39d16fda20388727c859e6
     first_name = event.get('first_name', '')
     middle_name = event.get('middle_name', '')
     last_name = event.get('last_name', '')
