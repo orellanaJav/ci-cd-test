1421c81e0f8c037742671bff2e8f9149e94001d6
Javier Orellana <javier.orellana@destacame.cl>
2022-06-22 16:03:00 -0400

    Added full stack

--- test_full_stack/services/test_1/core/test_1.py
+++ test_full_stack/services/test_1/core/test_1.py
@@ -24,6 +24,5 @@ def lambda_handler(event: dict, context: dict) -> dict:
 
     :Created:
         - 2022.06.22
-    """
-    logger.basic_loader(**event)
+    """    
     return {'status': 'status'}
