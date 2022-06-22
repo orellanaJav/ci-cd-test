63704a78f742b48174c203a5b41fe80b11fe93c4
Javier Orellana <javier.orellana@destacame.cl>
2022-06-21 20:28:28 -0400

    Added requirements

--- test_stack/services/test_1/core/test_1.py
+++ test_stack/services/test_1/core/test_1.py
@@ -27,8 +27,4 @@ def lambda_handler(event: dict, context: dict) -> dict:
     """
     a = np.arange(15).reshape(3, 5)
     print(a)
-    print(a)
-    print(a)
-    print(a)
-    print(a)
     return {'status': 'status'}
