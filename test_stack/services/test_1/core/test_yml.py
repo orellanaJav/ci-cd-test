ad8b06718e1846bee57771c0ad94403bd65bb9c4
Javier Orellana <javier.orellana@destacame.cl>
2022-06-22 12:41:59 -0400

    testing deploy by functions

--- test_stack/services/test_1/core/test_yml.py
+++ test_stack/services/test_1/core/test_yml.py
@@ -1,8 +1,12 @@
-from yaml import FullLoader
+from yaml import dump
 from yaml import load
+from yaml import FullLoader
 
 
 if __name__ == '__main__':
     with open('serverless.yml') as file:
         resource_data = load(file, Loader=FullLoader)
+        print(resource_data)
+        resource_data['plugins'].remove('serverless-python-requirements')
+        dump(resource_data, 'serverless.yml')
         print(resource_data)
