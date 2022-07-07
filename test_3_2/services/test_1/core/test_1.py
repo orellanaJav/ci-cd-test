3fa8aeacc85d3d49ce15ed00964720f3bf9c9fa8
Javier Orellana <javier.orellana@destacame.cl>
2022-07-07 19:31:44 -0400

    Updated Lambdas in news stacks

--- test_3_2/services/test_1/core/test_1.py
+++ test_3_2/services/test_1/core/test_1.py
@@ -27,4 +27,7 @@ def lambda_handler(event: dict, context: dict) -> dict:
     """
     logger.basic_loader(**event)
     print('holax')
+    print('holax')
+    print('holax')
+    print('holax')
     return {'status': 'status'}
