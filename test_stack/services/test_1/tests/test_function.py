271736da0a3e0adea280d6a00529100c0b9a04ec
Javier Orellana <javier.orellana@destacame.cl>
2022-07-11 19:20:25 -0400

    Added unittests

--- test_stack/services/test_1/tests/test_function.py
+++ test_stack/services/test_1/tests/test_function.py
@@ -11,7 +11,7 @@ class ExampleTestCase(unittest.TestCase):
     """
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FoxxxO')
+        self.assertEqual('foo'.upper(), 'FoxxO')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
