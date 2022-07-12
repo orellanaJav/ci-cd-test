9841df964f4a46ccf7ef3e25dfc20c0fda56a9f2
Javier Orellana <javier.orellana@destacame.cl>
2022-07-12 16:37:08 -0400

    Added new stack

---
+++ test_stack_bifrost/services/test_1/tests/unit_test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import unittest
+
+BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE)
+
+
+class ExampleTestCase(unittest.TestCase):
+    """
+    This is only an example of Unittest. You must develop your own.
+    """
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+
+if __name__ == '__main__':
+    unittest.main()
