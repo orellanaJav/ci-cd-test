ff85f0c890ed34bb98adb9c51938e0d3e087964b
Javier Orellana <javier.orellana@destacame.cl>
2022-07-21 15:48:23 -0400

    Added unittest validation

--- test/services/test_1/core/test_1.py
+++ test/services/test_1/core/test_1.py
@@ -26,9 +26,4 @@ def lambda_handler(event: dict, context: dict) -> dict:
         - 2022.07.21
     """
     logger.basic_loader(**event)
-    logger.basic_loader(**event)
-    logger.basic_loader(**event)
-    logger.basic_loader(**event)
-    logger.basic_loader(**event)
-    logger.basic_loader(**event)
     return {'status': 'status'}
