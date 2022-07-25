84f5dd25692f9ea13dafb8d93980a9e6add8517d
Javier Orellana <javier.orellana@destacame.cl>
2022-07-25 18:57:20 -0400

    Fix in tests

--- test_resources/services/test_2/tests/unit_test.py
+++ test_resources/services/test_2/tests/unit_test.py
@@ -6,7 +6,6 @@ BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE)
 
 # Agregar los imports de las funciones acá
-from core.test_2 import lambda_handler
 
 
 class ExampleTestCase(unittest.TestCase):
@@ -14,10 +13,6 @@ class ExampleTestCase(unittest.TestCase):
     Esto es solo un ejemplo de como debes realizar test unitarios a tus
     funciones.
     """
-
-    def test_lambda_handler(self):
-        handler = lambda_handler({}, {})
-        self.assertEqual(handler, {'status': 'status'})
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
