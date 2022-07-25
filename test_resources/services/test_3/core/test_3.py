a42ddb9211634ffc5c2088d74f41b9568c4c89cf
Javier Orellana <javier.orellana@destacame.cl>
2022-07-25 18:43:47 -0400

    Deleted and added stack

---
+++ test_resources/services/test_3/core/test_3.py
@@ -0,0 +1,29 @@
+from settings.config import logger
+
+__version__ = '0.1.0'
+
+
+def lambda_handler(event: dict, context: dict) -> dict:
+    """
+    Main
+
+    Parameters
+    ----------
+    event : dict
+        - idx: Identificador de tracking
+    context : dict
+
+    Returns
+    -------
+    dict
+        - status : str
+            Estado de la consulta
+
+    :Authors:
+        -
+
+    :Created:
+        - 2022.07.25
+    """
+    logger.basic_loader(**event)
+    return {'status': 'status'}
