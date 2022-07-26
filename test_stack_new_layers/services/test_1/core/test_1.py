f288c6eadbe4881d6a649a228494ed5c057dc4f3
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 12:25:01 -0400

    Added a new layer

---
+++ test_stack_new_layers/services/test_1/core/test_1.py
@@ -0,0 +1,31 @@
+from settings.config import logger
+from test_layer import test_layer
+
+__version__ = '0.1.0'
+
+
+def lambda_handler(event: dict, context: dict) -> dict:
+    """
+    Main
+
+    Parameters
+    ----------
+    event : dict
+        - idx: Identificador de tracking
+    context : dict
+
+    Returns
+    -------
+    dict
+        - status : str
+            Estado de la consulta
+
+    :Authors:
+        -
+
+    :Created:
+        - 2022.07.26
+    """
+    layer_return = test_layer()
+    logger.info('hola ---> ', layer_return)
+    return {'status': 'status'}
