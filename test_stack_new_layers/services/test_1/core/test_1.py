21dafca21722671c97dbe2b9a6d759a2e5d8e8c4
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 13:36:41 -0400

    Added new test

--- test_stack_new_layers/services/test_1/core/test_1.py
+++ test_stack_new_layers/services/test_1/core/test_1.py
@@ -28,4 +28,8 @@ def lambda_handler(event: dict, context: dict) -> dict:
     """
     layer_return = test_layer()
     logger.info('hola ---> ', layer_return)
+    logger.info('hola ---> ', layer_return)
+    logger.info('hola ---> ', layer_return)
+    logger.info('hola ---> ', layer_return)
+    logger.info('hola ---> ', layer_return)
     return {'status': 'status'}
