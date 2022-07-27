962ce341979df744ac4f4ba72b4f203e0921031f
Javier Orellana <javier.orellana@destacame.cl>
2022-07-26 21:58:22 -0400

    Added Test

--- test_stack_new_layers/services/test_1/core/test_1.py
+++ test_stack_new_layers/services/test_1/core/test_1.py
@@ -1,5 +1,8 @@
 
 from bifrost.connection_aws import invoker_dispatch
+from settings.config import logger
+from test_layer_bifrost import test_layer_bifrost
+
 
 __version__ = '0.1.0'
 
@@ -26,5 +29,7 @@ def lambda_handler(event: dict, context: dict) -> dict:
     :Created:
         - 2022.07.26
     """
-    # response = invoker_dispatch(function_name=connection, data=payload)
+    invoke = test_layer_bifrost({})
+    response = invoker_dispatch(function_name='test-stack-new-layers-test_2', region='sa-east-1', data={})
+    logger.info('resposne es ---> ', invoke)
     return {'status': 'status'}
