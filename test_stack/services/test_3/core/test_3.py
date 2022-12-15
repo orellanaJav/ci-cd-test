0706e44defbc9cc07b25cf17afda1af1a9544ffa
javore <javier.orellana@destacame.cl>
2022-12-15 10:55:54 -0300

    Minimal update

--- test_stack/services/test_3/core/test_3.py
+++ test_stack/services/test_3/core/test_3.py
@@ -28,4 +28,5 @@ def lambda_handler(event: dict, context: dict) -> dict:
     result = test()
     print('value', result)
     print('value', result)
+    print('value', result)
     return {'status': 'status'}
